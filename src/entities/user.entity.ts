07a2f56a5092f130d32a40913b2d1c388e612f54
salami <salamisodikiolawale@gmail.com>
2022-02-13 13:55:12 +0100

    creation du file entitie

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,36 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  userId: number;
+
+  @Column()
+  email:string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  username?: string;
+
+  @Column()
+  name?:string;
+
+  @Column()
+  phone?:string;
+
+  @Column()
+  skype?:string;
+
+  @Column()
+  lieu?:string;
+
+  @Column()
+  fuseau?:string;
+  
+  @Column()
+  anniversaire?:string
+}
+
